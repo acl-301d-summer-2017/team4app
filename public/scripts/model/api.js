778cc09c9e6696bc5cd0d8528da386a4b85e0c82
Zac-L <Zachlichens@gmail.com>
2017-09-19 15:54:36 -0700

    able to select between beer and brewery now through buttons in the search bar

--- public/scripts/model/api.js
+++ public/scripts/model/api.js
@@ -4,17 +4,35 @@ var test = '';
 var userInput = '';
 var url = '';
 
+
 $('#search_form').on('submit', function(){
     event.preventDefault();
     console.log('i have been clicked')
     test = $('#beer').val();
-    url =  test + '&type=beer';
+    var typeTest = test + '&type=';
+   var buttonChoice =  $('input[name=radio]:checked', '#search_form').val();
+   var selection = typeTest + buttonChoice;
+   console.log(selection)
+    // $('#brewery_choice').prop('checked', true) ? url =  test + '&type=brewery' : url = test + '&type=beer';
+    // if ($('#brewery_choice').prop('checked', true)){
+    //     url = test + '&type=brewery'
+    //     console.log('brewery was checked')
+    // }
+    // else if ($('#beer_choice').prop('checked', true)){
+    //     url = test + '&type=beer'
+    //     console.log('beer was checked')
+    // }
+    // else {
+    //      prompt('click a button')
+    // }
     $.get({
-        url: `/search/${url}`,
+        url: `/search/${selection}`,
         type: 'GET'
     })
     console.log(test);
     console.log(url);
 
 });
+
+
 
