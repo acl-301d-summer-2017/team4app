f912d7cfc7de243a23fca0f43e99d96da0cfdf68
Zac-L <Zachlichens@gmail.com>
2017-09-20 15:27:50 -0700

    handlebars is now working upon selection, and some other ish

--- public/scripts/model/breweryModel.js
+++ public/scripts/model/breweryModel.js
@@ -6,15 +6,10 @@ function Brewery(rawBrewery) {
     this.established = rawBrewery.established;
     this.isOrganic = rawBrewery.isOrganic;
     this.isMassOwned = rawBrewery.isMassOwned;
-    this.totalBeers = rawBrewery.totalResults - 1;
-    var brewTemplate = this.toHtml();
-    this.html = brewTemplate;
-    this.addToArr();
-    console.log(this.toHtml(), 'asdasd');
-    console.log(this.html, 'aasdgdfhdhdghdhfsd')
-
-
-
+    this.description = rawBrewery.description;
+    this.getBeerList();
+    this.addToOptions();
+    console.log(this)
 }
 
 Brewery.all = [];
@@ -27,3 +22,45 @@ Brewery.prototype.toHtml = function() {
     let template = Handlebars.compile($('#brewery-template').text());
     return template(this)
 }
+
+Brewery.prototype.getBeerList = function () {
+    $.get({
+        url: `/brewery/${this.id}`,
+        type: 'GET'
+    })
+    .then( dataObj => {
+        this.beerCount = dataObj.length;
+        console.log('beerCount' , dataObj.length);
+        var brewTemplate = this.toHtml();
+        this.html = brewTemplate;
+        this.addToArr();
+        this.saveToLocal();
+        breweryAppend(this);
+    })
+}
+
+Brewery.prototype.addToOptions = function() {
+    
+        console.log(Beer.all, 'something');
+        var $option = $('<option></option>').text(this.name);
+        var $option2 = $('<option></option>').text(this.name);
+        $('#select_one').append($option);
+        $('#select_two').append($option2);
+    
+    };
+
+Brewery.prototype.saveToLocal = function() {
+        var localSavedData = JSON.stringify(this);
+        localStorage.setItem(( Brewery.all.length * 2 ) + 1 , localSavedData);
+    };
+
+    if (localStorage) {
+        for (var i = 1; i < localStorage.length; i++) {
+            if (localStorage.getItem( ( i * 2 ) + 1 ) ) {
+                var savedBrewery = JSON.parse(localStorage.getItem( (i * 2) + 1 ) );
+                console.log('index' , (i * 2) + 1 )
+                console.log(savedBrewery);
+                new Brewery(savedBrewery);
+            }
+        }
+    }
--- public/scripts/view/beerView.js
+++ public/scripts/view/beerView.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function beerAppendOne(dataObj) {
+    $('#option_one').append(dataObj.html);
+    console.log('beerAppend' , dataObj.html);
+}
+
+function beerAppendTwo(dataObj) {
+    $('#option_two').append(dataObj.html);
+    console.log('beerAppend' , dataObj.html);
+}
